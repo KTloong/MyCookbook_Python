6678bd8c920f987fdaee23b4414d11f44a61cb06
KTloong <caiyunfengxiao@gmail.com>
2019-06-11 21:34:19 +0800

    从一大堆文件夹中提取指定文件! 我觉得这段代码我写的挺好的哈哈哈

--- GetFilesFromFolders.py
+++ GetFilesFromFolders.py
@@ -10,11 +10,11 @@ folder_paths = [os.path.join(root_path, fpath) for fpath in folders]
 p_collection = []
 
 for fp in folder_paths:
-    temp = [fpath for fpath in os.listdir(fp) if "0007" in fpath or "0008" in fpath]
+    temp = [fpath for fpath in os.listdir(fp) if "0003" in fpath or "0004" in fpath]
     p_t = [os.path.join(fp, spath) for spath in temp]
     p_collection = p_collection + p_t
 
-newdir = r"C:\Users\kingl\Desktop\dorm_2"
+newdir = r"C:\Users\kingl\Desktop\CBD"
 
 for oldname in p_collection:
     base = os.path.basename(oldname)
